6dc4bfbd0c4ff52c4469376f7ce26d86414295d4
Saarthak Slathia <77338782+Saarthak-Slathia@users.noreply.github.com>
2021-08-17 18:31:53 +0530

    Create main.kt

---
+++ main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+
+    val first: Int = 21
+    val second: Int = 3
+
+//   calculating the quotient
+    val quotient = first / second
+
+//   printing the quotient
+    println("The quotient is: $quotient")
+
+}
